e9088a2e628c2088838fd514dd8c97ae5f7895b1
THAI-Nicolas <nicolas.thai@edu.univ-fcomte.fr>
2025-01-31 11:55:49 +0100

    Boutton pour changer l'affichage en grille ou en colonne

--- src/backend.mjs
+++ src/backend.mjs
@@ -2,7 +2,6 @@ import PocketBase from 'pocketbase';
 const pb = new PocketBase('http://127.0.0.1:8090');
 
 export async function getOffres() {
-    console.log('Lecture des offres');
     try {
         let data = await pb.collection('maison').getFullList({
             sort: '-created',
@@ -11,7 +10,6 @@ export async function getOffres() {
             uneMaison.img = pb.files.getURL(uneMaison, uneMaison.images_maison);
             return uneMaison;
         });
-        console.log('Liste des maisons', data);
         return data;
     } catch (error) {
         console.log('Une erreur est survenue en lisant la liste des maisons', error);
